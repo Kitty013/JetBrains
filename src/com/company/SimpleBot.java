6421987865d63dd057cdc0e632a6ff2e1700c4ed
Kinky <kinkyk013@gmail.com>
2022-04-10 10:49:20 +0200

    Completed stage 3 of the project - update project with guess age RelationalOperators and update project

--- src/com/company/SimpleBot.java
+++ src/com/company/SimpleBot.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class SimpleBot {
 
     public static void main(String[] args) {
 		Bot bot = new Bot();
@@ -14,5 +14,13 @@ public class Main {
 
 		String name = scanner.nextLine();
 		System.out.println("What a great name you have, " + name + "!");
+
+		System.out.println("Let me guess your age.");
+		System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+		int remainder3 = scanner.nextInt();
+		int remainder5 = scanner.nextInt();
+		int remainder7 = scanner.nextInt();
+		int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+		System.out.println("Your age is " + age);
     }
 }

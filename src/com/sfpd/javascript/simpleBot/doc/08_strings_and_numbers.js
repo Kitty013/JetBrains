c528cf6b4208ca9a96d796198413dfc0547f825a
Kitty013 <kittyDuc013@gmail.com>
2022-12-25 23:39:46 +0100

    comments - strings - numbers - variables - arithmetic operators - functions

---
+++ src/com/sfpd/javascript/simpleBot/doc/08_strings_and_numbers.js
@@ -0,0 +1,30 @@
+/**
+$1. Strings
+    Strings are written in single or double quotes.
+**/
+    console.log("");             // empty string
+    console.log("string");       // one word
+    console.log("Hello, world"); // a phrase
+
+    console.log('a');    // single character
+    console.log('1234'); // a sequence of digits
+
+/**
+$2. Numbers
+    You can use positive, negative and zeros. Integer numbers can be used to count
+    physical objects, while floating-point numbers are a good choice for statistical
+    and scientific calculations.
+**/
+    console.log(12);
+    console.log(0);
+    console.log(-11);
+    console.log(-1.04);
+
+/**
+$3. typeof operator
+    We can easily recognize the data type using the typeof operator. There are two ways
+    to write this operator.
+**/
+    console.log(typeof 9); // number
+    console.log(typeof(9)); // number
+

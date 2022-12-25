364f5f51d305f81dbcb67ecf8fc69306c3446ddb
Kitty013 <kittyDuc013@gmail.com>
2022-12-25 17:23:13 +0100

    web development

--- src/com/sfpd/javascript/simpleBot/doc/03_web_development.js
+++ src/com/sfpd/javascript/simpleBot/doc/03_web_development.js
@@ -19,5 +19,25 @@ $2. Developers
     Full-stack developers: are involved in all phases of web application development.
     From the creation of the client part to the implementation of the server part.
 
+$3. Separation of client and servers parts
+    - we can independently replace the frontend and backend logic, minimizing the
+      number of errors
+    - we need a server to store and structure data
+    - data on the client's side may become outdated, but the server's answer are
+      usually up-to-date
+    - it's easier to manage data flows and orchestrate processes on a few servers
+      than on millions of clients
+    - some tasks don't need an update from a server, it's faster to perform them
+      locally
 
+$4. How do frontend and backend communicate
+    This happens through HTTP requests:
+    1. The client sends a request to the server to retrieve of modify data using the
+       HTTP protocol.
+    2. Backend processes this request and returns the response via HTTP.
+    3. Frontend receives the response and processes it, and then displays the result
+       to the user.
+
+    The most popular format for exchanging data between client and server is JSON.
+    The most common used markup languages are HTML and XML
 /**

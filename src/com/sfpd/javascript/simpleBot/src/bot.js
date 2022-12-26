88b67ef3fde0a41753425a3dd4e8df295b5acd63
Kitty013 <kittyDuc013@gmail.com>
2022-12-26 20:41:08 +0100

    Finished Stage 3/5: Guess your age

--- src/com/sfpd/javascript/simpleBot/src/bot.js
+++ src/com/sfpd/javascript/simpleBot/src/bot.js
@@ -5,9 +5,7 @@ $1. Description
     intelligence with whom you can talk.
     In this project, you will develop step by step a simple bot that will help you
     study programming.
-**/
 
-/**
 $2. Objective
     For the first stage, you will write a bot who displays a greeting, its name,
     and the date of its creation. First impressions count!
@@ -16,20 +14,123 @@ $2. Objective
         I was created in {birth_year}.
     Instead of {bot_name}, print any name you choose and replace {birth_year}
     with the current year (four digits).
-**/
 
-/**
 $3. Example
         Hello! My name is Aid.
         I was created in 2022.
     You can change the text if you want but print exactly two lines.
     Next, we will use Aid and 2022 as your bot's name and its birth year, but you
     can change it if you need to.
-/**
+**/
 
 let botName = "Kitty";
 const greeting = "Hello! My name is " + botName;
+
 let age = 2022;
 const creation = "I was created in " + age;
+
 console.log(greeting);
 console.log(creation);
+
+/**
+$1. Description
+    The greeting part is great, but chatbots are also supposed to interact with
+    a user. It's time to implement this functionality.
+
+$2. Objective
+    In this stage, you will introduce yourself to the bot so that it can greet you
+    by your name.
+    Your program should print the following lines:
+
+    Hello! My name is Aid.
+    I was created in 2022.
+    Please, remind me your name.
+    What a great name you have, {your_name}!
+    You may change the name and the creation year of your bot if you want.
+
+    Instead of {your_name}, the bot must print your name entered from the standard
+    input.
+
+$3. Example
+    The greater-than symbol followed by a space (> ) represents the user input.
+    Note that it's not part of the input.
+    Example 1: a dialogue with the bot
+
+    Hello! My name is Aid.
+    I was created in 2022.
+    Please, remind me your name.
+    > Max
+    What a great name you have, Max!
+
+    Use the provided template to simplify your work. You can change the text, but
+    not the number of printed lines.
+**/
+    const input = require('sync-input');
+
+    console.log("Hello! My name is Kitty.");
+    console.log("I was created in 2022.");
+    console.log("Please, remind me your name.");
+    let name = input();
+    // use input() + to get the user's output
+    console.log("What a great name you have, " + name + "!");
+
+/**
+$1. Description
+    Keep improving your bot by developing new skills for it. We suggest a simple
+    guessing game that will predict the age of a user.
+    It's based on a simple math trick. First, take a look at this formula:
+        age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    The numbers remainder3, remainder5, and remainder7 are the remainders of division
+    by 3, 5 and 7 respectively.
+    It turns out that for each number ranging from 0 to 104, the calculation will
+    result in the number itself. This perfectly fits the ordinary age range,
+    doesn't it? Ask the user for the remainders and use them to guess the age!
+
+$2. Objective
+    In this stage, you will introduce yourself to the bot. It will greet you by
+    your name and then try to guess your age using arithmetic operations.
+    Your program should print the following lines:
+        Hello! My name is Aid.
+        I was created in 2022.
+        Please, remind me your name.
+        What a great name you have, Max!
+        Let me guess your age.
+        Enter remainders of dividing your age by 3, 5 and 7.
+        Your age is {your_age}; that's a good time to start programming!
+    Read three numbers from the standard input. Assume that all the numbers will
+    be given on separate lines.
+    Instead of {your_age}, the bot will print the age determined according to the
+    special formula discussed above.
+
+$3. Example
+    The greater-than symbol followed by a space (> ) represents the user input.
+    Note that it's not part of the input.
+    Example 1: a dialogue with the bot
+        Hello! My name is Aid.
+        I was created in 2022.
+        Please, remind me your name.
+        > Max
+        What a great name you have, Max!
+        Let me guess your age.
+        Enter remainders of dividing your age by 3, 5 and 7.
+        > 1
+        > 2
+        > 1
+        Your age is 22; that's a good time to start programming!
+**/
+    const input = require('sync-input');
+
+    console.log("Hello! My name is Kitty.");
+    console.log("I was created in 2022.");
+    console.log("Please, remind me your name.");
+
+    let name = input();
+    console.log("What a great name you have, " + name + "!");
+    console.log("Let me guess your age.");
+    console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+    let remainder3 = input();
+    let remainder5 = input();
+    let remainder7 = input();
+    let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+    console.log("Your age is " + age + " that's a good time to start programming!");

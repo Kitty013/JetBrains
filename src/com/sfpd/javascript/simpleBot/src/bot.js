77ce18748678c92ae726512312edab5826eaba3a
Kitty013 <kittyDuc013@gmail.com>
2022-12-28 16:32:38 +0100

    Finished Stage 5/5

--- src/com/sfpd/javascript/simpleBot/src/bot.js
+++ src/com/sfpd/javascript/simpleBot/src/bot.js
@@ -201,3 +201,124 @@ $3. Example
     };
 
     console.log("Completed, have a nice day!");
+
+/**
+$1. Description
+    At the final stage, you will improve your simple bot so that it can give you a
+    test and check your answers. The test should be a multiple-choice quiz about
+    programming with any number of options. Your bot has to repeat the test until
+    you answer correctly and congratulate you upon completion.
+
+$2. Objective
+    Your bot can ask anything you want, but there are two rules for your output:
+     -   the line with the test should end with the question mark character;
+     -   an option starts with a digit followed by the dot (1., 2., 3., 4.)
+    If a user enters an incorrect answer, the bot may print a message:
+            Please, try again.
+    The program should stop on the correct answer and print Congratulations, have
+    a nice day! at the end.
+
+$3. Example
+    The greater-than symbol followed by a space (> ) represents the user input.
+    Note that it's not part of the input.
+    Example 1: a dialogue with the final version of your bot
+        Hello! My name is Aid.
+        I was created in 2022.
+        Please, remind me your name.
+        > Max
+        What a great name you have, Max!
+        Let me guess your age.
+        Enter remainders of dividing your age by 3, 5 and 7.
+        > 1
+        > 2
+        > 1
+        Your age is 22: that's a good time to start programming!
+        Now I will prove to you that I can count to any number you want.
+        > 3
+        0 !
+        1 !
+        2 !
+        3 !
+        Let's test your programming knowledge.
+        Why do we use methods?
+        1. To repeat a statement multiple times.
+        2. To decompose a program into several small subroutines.
+        3. To determine the execution time of a program.
+        4. To interrupt the execution of a program.
+        > 4
+        Please, try again.
+        > 2
+        Congratulations, have a nice day!
+    The program must end with the Congratulations, have a nice day! message.
+    Use the provided template to simplify your work. You can change the text if
+    you want. Please note that we use functions to make it easy to understand the
+    program and add new code to it or edit later.
+**/
+    const input = require('sync-input');
+
+
+    function greet(bot_name, birth_year) {
+    	console.log("Hello! My name is " + bot_name + ".");
+    	console.log("I was created in " + birth_year + ".");
+    }
+
+    function remind_name() {
+    	console.log("Please, remind me your name.");
+    	let name = input();
+    	console.log("What a great name you have, " + name + "!");
+    }
+
+    function guess_age() {
+    	console.log("Let me guess your age.");
+    	console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+    	let rem3 = Number(input());
+    	let rem5 = Number(input());
+    	let rem7 = Number(input());
+
+    	let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+    	console.log("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    function count() {
+    	console.log("Now I will prove to you that I can count to any number you want.");
+
+    	let number = Number(input());
+    	let current = 0;
+
+    	while (current <= number) {
+    		console.log(current + " !");
+    		current += 1;
+    	}
+    }
+
+    function question() {
+         console.log("Why do we use methods?");
+         console.log("1. To repeat a statement multiple times.");
+         console.log("2. To decompose a program into several small subroutines.");
+         console.log("3. To determine the execution time of a program.");
+         console.log("4. To interrupt the execution of a program.");
+    }
+
+    function test() {
+    	console.log("Let's test your programming knowledge.");
+    	question();
+    	let response = input();
+    	if (response == 2 || response == 0) {
+    	    end();
+    	} else {
+    	    console.log("Please, try again.");
+    	};
+    }
+
+    function end() {
+    	console.log("Congratulations, have a nice day!");
+    }
+
+
+    greet('Kitty', '2022')  // change it as you need
+    remind_name();
+    guess_age();
+    count();
+    test();
